81cd6b72aaddf692a92ff4744ff4ddeabc68349d
Pedro Felipe <pedrofelipesds2003@gmail.com>
2024-01-28 15:33:43 -0300

    Configurações de consumo da API e modifições na estilização geral do site

--- script.js
+++ script.js
@@ -3,32 +3,34 @@ const resultsArtists = document.getElementById('result-artist');
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
-    fetch(url)
+    // const url = `http://localhost:3000/artists?name_like=${searchTerm}` 
+    fetch(`http://localhost:3000/artists?name_like=${searchTerm}`)
         .then((response) => response.json())
-        .then((result) => displayResults(result))
+        .then((result) => displayResults(result));
 } 
 
 function displayResults(result) {
-    resultPlaylist.classList.add('hidden');
-    const artistName = document.getElementById('.artist-name');
-    const artistImage = document.getElementById('.artist-img');
+    hidePlaylists();
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
 
-    result.forEach(element => {
+    result.forEach((element) => {
         artistName.innerText = element.name;
         artistImage.src = element.urlImg;
     });
-
     resultsArtists.classList.remove('hidden');
+}
+
+function hidePlaylists() {
+    resultPlaylist.classList.add('hidden');
 }
 
 document.addEventListener("input", function() {
     const searchTerm = searchInput.value.toLowerCase();
-    if (searchTerm === "") {
-        resultPlaylist.classList.add('hidden');
-        resultsArtists.classList.remove('hidden');
+    if (searchTerm === '') {
+        resultPlaylist.classList.remove('hidden');
+        resultsArtists.classList.add('hidden');
         return;
     }
-
     requestApi(searchTerm);
-})
+});
